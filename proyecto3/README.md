fc63cf6fbbfc3ee01529642f3fd248c16caa91ad
Beth Saav <bethsaav@ciencias.unam.mx>
2025-03-03 17:48:21 -0600

    versión final

--- proyecto3/README.md
+++ proyecto3/README.md
@@ -1,3 +1,3 @@
 # **Arreglos y Ciclos**
 
-Este proyecto tiene como objetivo reforzar y aplicar los siguientes conceptos: ciclo while, ciclo for y arrays (arreglos)
+Este proyecto tiene como objetivo reforzar y aplicar los siguientes conceptos: ciclo while, ciclo for y arrays (arreglos).
--- proyecto3/ejemplo-for.js
+++ proyecto3/ejemplo-for.js
@@ -1,9 +1,24 @@
 //Declalar un array que contenga frutas
 
-let frutas = ["manzana", "sandía", "uva", "tuna","jícama", "kiwi", "melón"];
+let frutas = ["manzana", "kiwi", "melón", "manzana", "sandía", "kiwi", "manzana", "tuna", "jícama", "uva", "tuna","jícama", "kiwi", "melón"];
 
 //Crear un objeto
 
 let contadorDeFrutas = {};
 
 //Recorrer el array para contar las frutas
+
+for (let i = 0; i < frutas.length; i++) {
+    let fruta = frutas[i];
+    if (contadorDeFrutas[fruta]) {
+        contadorDeFrutas[fruta]++;
+    } else {
+        contadorDeFrutas[fruta] = 1;
+    }
+}
+console.log("Conteo de frutas:", contadorDeFrutas);
+
+
+
+
+
