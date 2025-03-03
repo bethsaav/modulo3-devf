6dbba53d87da24b86bf24d63031ae5ec9080a9c8
Beth Saav <bethsaav@ciencias.unam.mx>
2025-03-02 19:38:17 -0600

    versión final

---
+++ proyecto1/tipos-de-datos.js
@@ -0,0 +1,40 @@
+// Datos primitivos
+
+let edad = 42;
+let edadCarla = "Venticinco";
+let ingresosPerdidos = -666;
+let esEstudiante = true;
+let materiasReprobadas = 0;
+let pruebaSimbolo = "";
+let existe = null;
+let prueba = undefined;
+let gatosAzules = false;
+
+// Imprimir los tipos de datos
+
+console.log(typeof edad);
+console.log(typeof edadCarla);
+console.log(typeof ingresosPerdidos);
+console.log(typeof esEstudiante);
+console.log(typeof materiasReprobadas);
+console.log(typeof pruebaSimbolo);
+console.log(typeof existe);
+console.log(typeof prueba);
+console.log(typeof gatosAzules);
+
+
+
+//Pruebas
+let nombre = "Elizabeth"; //string
+let edadEli = 24; //number
+let carrera = "Actuaria"; //string
+let nacionalidad = "Mexicana";  //string
+let tieneMascotas = true; //boolean
+
+// Imprimir las variables prueba
+
+console.log(typeof nombre);
+console.log(typeof edadEli);
+console.log(typeof carrera);
+console.log(typeof nacionalidad);
+console.log(typeof tieneMascotas);
