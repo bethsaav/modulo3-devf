2524605279972d16f27b3bf58b13fbc5bcbc6a79
Beth Saav <bethsaav@ciencias.unam.mx>
2025-03-03 14:30:20 -0600

    prueba ejercicio

--- proyecto2/comparaciones.js
+++ proyecto2/comparaciones.js
@@ -1,1 +1,20 @@
-//
+//Declaramos la variable
+
+let calificacion = Number(prompt('Ingresa tu calificacion'));
+
+//Realizamos la condicional
+
+if(calificacion >0){
+    if(calificacion >=90){
+        console.log("Excelente");
+    }else if(calificacion >=75 && calificacion <=89){
+        console.log("Bien");
+    }else if(calificacion >=60 && calificacion <=74){
+        console.log("Suficiente");
+    }else if(calificacion <60){
+        console.log("No aprobaste");
+    }
+}else{
+    console.log("Ingresa una calificacion valida");
+    
+}
