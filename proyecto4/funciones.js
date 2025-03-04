faeca05cca4458abbdd0d8d272fd6d57a2f9ae1f
Beth Saav <bethsaav@ciencias.unam.mx>
2025-03-03 19:40:43 -0600

    edición archivos

---
+++ proyecto4/funciones.js
@@ -0,0 +1,15 @@
+//Declarar un array
+
+let librosLeidos = [];
+
+//Definir función para agregar libros
+function agregarLibro(titulo){
+    librosLeidos.push(titulo);
+    console.log('El libro ' + titulo + ' ha sido agregado ');
+}
+
+//Definir función para mostrar libros leidos
+
+function mostrarLibrosLeidos(){
+    
+}
