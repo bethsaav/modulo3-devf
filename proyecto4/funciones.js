908d7f04c543059cdce60312e2addbad77759fe1
Beth Saav <bethsaav@ciencias.unam.mx>
2025-03-04 18:57:09 -0600

    primer prueba

--- proyecto4/funciones.js
+++ proyecto4/funciones.js
@@ -11,5 +11,18 @@ function agregarLibro(titulo){
 //Definir función para mostrar libros leidos
 
 function mostrarLibrosLeidos(){
-    
+    if(librosLeidos.length === 0){
+        console.log('No has leido ningun libro');
+    }else{
+        console.log('Libros leidos:');
+        librosLeidos.forEach((libro, index) => {
+            console.log(`${index + 1}. ${libro}`);
+        })
+    }
 }
+
+agregarLibro('Powerless');
+agregarLibro('Los juegos del hambre');
+agregarLibro('Vidas Cruzadas');
+agregarLibro('Gardenia');
+mostrarlibrosLeidos();
